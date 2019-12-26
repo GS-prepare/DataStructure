a79b6a5468290fece772dfac2e5112115127fec9
sunnu06 <sunnu06@icloud.com>
2019-12-26 16:06:30 +0800

    initial

---
+++ Bsaic_DS/Bsaic_DS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bsaic_DS
+//
+//  Created by Sun on 2019/12/26.
+//  Copyright © 2019 Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
